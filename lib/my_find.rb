736463a9d5007fdcf9340afa08f52892478f8104
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 04:18:51 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,10 @@
 def my_find(collection)
   i = 0 
    while i < collection.length
-    return collection[i] if 
-    yield (collection[i]) 
-   i =+ 1 
+   if yield(collection[i])
+     return collection[i]
+   end 
+    i = i + 1
    end 
 end
 
